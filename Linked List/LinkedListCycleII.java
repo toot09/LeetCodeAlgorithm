50131728217fc4ab42d8a0214e56f1476c6e2475
UJONE <03toot28@gmail.com>
2020-07-06 13:09:08 +0000

    2020-07-06 At home. My wife uses foot massage machine because of her leg numbness.

---
+++ Linked List/LinkedListCycleII.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+/*
+xxxxxx
+  x  x
+  xxxx
+A : 2 (Loop전까지 step)
+B : 10 (Loop step)
+X : walker 와 runner가 만난 step
+walker = A + X
+runner = A + X + B*n
+2*walker = runner
+A + X = B
+*/
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null) return null;
+        ListNode walker = head;
+        ListNode runner = head;
+        while(runner.next != null) {
+            runner = runner.next;
+            if(runner.next != null) {
+                runner = runner.next;
+                walker = walker.next;
+                if(walker == runner) {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        if(runner.next == null) {
+            return null;
+        }
+        runner = head;
+        while(runner != walker) {
+            runner = runner.next;
+            walker = walker.next;
+        }
+        return walker;
+    }
+}
--- Main.java
+++ Main.java
@@ -3,8 +3,25 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
   
-  List<Integer> list = new ArrayList<>();
-    
+  
+ /* Your MyLinkedList object will be instantiated and called as such:
+ */
+  MyLinkedList obj = new MyLinkedList();
+  obj.addAtHead(1);
+  obj.addAtTail(3);
+  System.out.println(obj.get(0));
+  System.out.println(obj.get(1));
+  System.out.println("@@@@");
+  
+  obj.addAtIndex(1,2);
+  System.out.println(obj.get(0));
+  System.out.println(obj.get(1));
+  System.out.println(obj.get(2));
+  
+  //obj.deleteAtIndex(1);
+  //System.out.println(obj.get(1));
+ 
+
   }
 
 
