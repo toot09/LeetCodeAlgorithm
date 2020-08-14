a99055c53ef48e3fdfb7a62eecbadd1ba2fec6b4
UJONE <03toot28@gmail.com>
2020-08-14 13:35:20 +0000

    2020-08-14 Friday is always makes me happy. but this weekend there is a Disk Storage transform work. so I'll go to work on sunday early morning.

--- Main.java
+++ Main.java
@@ -4,7 +4,12 @@ class Main {
   
   public static void main(String[] args) {
     System.out.println("##### Compile Complete! #####");
+    //String s = "3[a]2[bc]"; //Output: "aaabcbc"
+    //String s = "3[a2[c]]"; //Output: "accaccacc"
+    //String s = "2[abc]3[cd]ef"; //Output: "abcabccdcdcdef"
+    //String s = "abc3[cd]xyz"; //Output: "abccdcdcdxyz"
   }
+
 }
 
 
