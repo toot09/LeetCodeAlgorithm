75ef0a3ca6ae7a43135876282936cd1aa529dc2d
UJONE <03toot28@gmail.com>
2020-09-09 12:59:15 +0000

    2020-09-09 Have to study about Lamda and compare

--- Main.java
+++ Main.java
@@ -5,11 +5,32 @@ class Main {
   public static void main(String[] args) {
     System.out.println("##### Start! #####");
 
-    groupShiftedStrings test = new groupShiftedStrings();
-    String[] str = {"z","abc","bcd","ba","xyz","a","az","acef"};
-    test.groupStrings(str);
+    int[] nums = {1,1,1,2,2,3};
+    int k = 2;
+    for(int i : topKFrequent(nums,k)) {
+      System.out.println(i);
+    }
 
     System.out.println("##### Compile Complete #####");
+  }
+
+  public static int[] topKFrequent(int[] nums, int k) {
+      PriorityQueue<Map.Entry<Integer, Integer>> q = new PriorityQueue<>();
+      Map<Integer, Integer> hm = new HashMap<>();
+      for(int num : nums) {
+        hm.put(num,hm.getOrDefault(num,0)+1);
+      }
+      for(Map.Entry<Integer, Integer> e : hm.entrySet()) {
+        q.offer(e);
+        if(q.size()>k) {
+          q.poll();
+        }
+      }
+      int[] ret = new int[k];
+      for(int i=0 ; i<k ; i++) {
+        ret[i] = q.poll().getKey();
+      }
+      return ret;
   }
 
 }
